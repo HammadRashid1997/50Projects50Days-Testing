ed58d8e91fc581bcb51d424e12e43c60e2a6e9d0
Muhammad Hammad Rashid <160396261+HammadRashid1997@users.noreply.github.com>
2024-02-29 12:04:15 +0500

    Create test_homepage.py

---
+++ button-riplle-effect/test/test_homepage.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+# Define Chrome WebDriver options
+chrome_options = Options()
+service = Service('/Users/mac005/Downloads/chromedriver-mac-arm64/chromedriver')
+
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get('http://127.0.0.1:5500/button-ripple-effect/')  
+
+element = driver.find_element(By.TAG_NAME, 'body')
+
+bgcolor = element.value_of_css_property('background-color')
+
+time.sleep(5)
+
+print('Successful opening of the page')
+
+print(f'The background color is {bgcolor}')
+
+# Close the browser window
+driver.quit()
