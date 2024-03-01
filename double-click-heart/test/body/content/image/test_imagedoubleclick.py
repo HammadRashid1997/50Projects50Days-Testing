5b941684bb9e49c79175cfe600d4b6fc5eea3fa7
Muhammad Hammad Rashid <160396261+HammadRashid1997@users.noreply.github.com>
2024-03-01 12:10:52 +0500

    Add files via upload

---
+++ double-click-heart/test/body/content/image/test_imagedoubleclick.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+# Define Chrome WebDriver options
+chrome_options = Options()
+# chrome_options.add_argument("--headless")  # Run headless if needed
+service = Service('/Users/mac005/Downloads/chromedriver-mac-arm64/chromedriver')
+
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get('http://127.0.0.1:5500/double-click-heart')
+
+element = driver.find_element(By.CLASS_NAME, 'loveMe')
+
+action = ActionChains(driver)
+action.double_click(element)
+
+time.sleep(3)
+
+print('The double click has been done successfully.')
+driver.quit()
