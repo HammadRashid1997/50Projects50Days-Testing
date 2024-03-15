498bce778dfe9f3089764c161e555139796c458d
DevQA2001 <157138433+DevQA2001@users.noreply.github.com>
2024-03-15 08:57:22 +0500

    Create test_text.py

---
+++ sticky-navigation/test/body/container_one/test/test_text.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+# Define Chrome WebDriver options
+chrome_options = Options()
+service = Service('/Users/mac005/Downloads/chromedriver-mac-arm64/chromedriver')
+
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get('http://127.0.0.1:5500/sticky-navigation/')  
+
+element = driver.find_element(By.XPATH, '//div[@class="hero"]//p')
+
+color = element.value_of_css_property('color')
+
+time.sleep(5)
+
+if element:
+    print('text found')
+else:
+    print('text not found')
+    
+print(f'The color of the text is: {color}')
+
+# Close the browser window
+driver.quit()
